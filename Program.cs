ea1bacb3c99a96697d84e22b8e5735c0a8faa14a
Oppong Richard <nanajr911@gmail.com>
2026-07-01 14:47:27 +0000

    Display the student results

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp5
     {
         static void Main(string[] args)
         {
-            string studfname, studlname, studDepartment, StudProgram, courseOfStudy;
+            string studfname, studlname, studDepartment, StudProgram, courseOfStudy, grade;
             double midsemester, finalexam,classtest,totalmarks;
 
             // Prompt the user to enter student details
@@ -33,9 +33,44 @@ namespace ConsoleApp5
             midsemester = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Enter student Final Exam marks: ");
             finalexam = Convert.ToDouble(Console.ReadLine());
-          
+            Console.WriteLine();
+
+            // Calculate total marks
+            totalmarks = midsemester + finalexam + classtest;
+
+            // Determine the grade based on total marks
+            if (totalmarks >= 80)
+            {
+                grade = "A";
+            }
+            else if (totalmarks >= 70)
+            {
+                grade = "B";
+            }
+            else if (totalmarks >= 60)
+            {
+                grade = "C";
+            }
+            else if (totalmarks > 50)
+            {
+                grade = "D";
+            }
+            else
+            {
+                grade = "F";
+            }
+
+            // Display the student results
+            Console.WriteLine("Student Name: " + studfname + " " + studlname);
+            Console.WriteLine("Department: " + studDepartment);
+            Console.WriteLine("Program of Study: " + StudProgram);
+            Console.WriteLine("Course of Study: " + courseOfStudy);
+            Console.WriteLine("Total Marks: " + totalmarks);
+            Console.WriteLine("Grade: " + grade);
+            Console.ReadLine();
 
 
         }
     }
 }
+    
