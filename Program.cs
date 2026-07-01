f8b2df8336e491af510015768244a935562cb31c
Oppong Richard <nanajr911@gmail.com>
2026-07-01 14:22:09 +0000

    Enter student details

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// A console Application which displays students results
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,21 @@ namespace ConsoleApp5
     {
         static void Main(string[] args)
         {
+            string studfname, studlname, studDepartment, StudProgram, courseOfStudy;
+            double midsemester, finalexam,classtest,totalmarks;
+
+            // Prompt the user to enter student details
+            Console.WriteLine("Enter Student First Name: ");
+            studfname= Console.ReadLine();
+            Console.WriteLine("Enter Student Last Name: ");
+            studlname= Console.ReadLine();
+            Console.WriteLine("Enter student Department");
+            studDepartment= Console.ReadLine();
+            Console.WriteLine("Enter student Program of study");
+            StudProgram= Console.ReadLine();
+            Console.WriteLine("Enter student Course of study");
+            courseOfStudy= Console.ReadLine();
+
         }
     }
 }
